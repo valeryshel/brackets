e8530723e49274497dc20933dfc3189a2814ed80
valeryshel <valeryshel@gmail.com>
2021-02-25 12:35:21 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const collection = bracketsConfig.map( el => el.join("") ); // получаем набор скобок вида () [] {}  и т.п.
+
+    for (let i = 0; i < collection.length; i++) {
+      if( str.includes(collection[i]) ) {// проверка на совпадающие элементы
+        str = str.replace(collection[i], ""); // замена совпадающих элементов на пустую строку
+        i = -1;
+      }
+    }
+    return  !str; // если в str что-то останется вернется false
 }
